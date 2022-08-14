f63d785b0cbb0a642a44c72790a3db476d27e58e
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-08-15 00:46:35 +0200

    dice numbers

---
+++ src/Die.js
@@ -0,0 +1,10 @@
+import './Main.css';
+import React from "react";
+
+export default function Die(props) {
+  return (
+    <div className='single-dice'>
+      <h4>{props.value}</h4>
+    </div>
+  )
+}
--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import './Main.css';
+import Die from "./Die"
 import React from "react"
 
 export default function Main() {
@@ -6,6 +7,14 @@ export default function Main() {
     <>
       <main>
         <h1 className='title'>Tengame</h1>
+        <div className='dices-container'>
+          <Die value={1} />
+          <Die value={2} />
+          <Die value={3} />
+          <Die value={3} />
+          <Die value={3} />
+          <Die value={3} />
+        </div>
       </main>
     </>
   )
