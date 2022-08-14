4b23f101b04ccf37ebca4f652fe7d6108b5e3ae1
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-08-14 13:07:11 +0200

    basic css

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+import React from "react"
+
+export default function Main() {
+  return (
+    <>
+      <main>
+        <h1 className='title'>Tengame</h1>
+      </main>
+    </>
+  )
+}
