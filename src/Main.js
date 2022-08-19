87b0e417c81c20719b48dbce2e8fa6e355efbc32
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-08-19 20:48:25 +0200

    random dice fuction

--- src/Main.js
+++ src/Main.js
@@ -3,21 +3,23 @@ import Die from "./Die"
 import React from "react"
 
 export default function Main() {
+  function allNewDice() {
+    const randomNum = [];
+    for (let i = 0; i < 10; i++) {
+      const num = Math.random() * 6;
+      randomNum.push(Math.ceil(num));
+    }
+    return randomNum
+  }
+
+  console.log(allNewDice())
+
   return (
     <>
       <main>
         <h1 className='title'>Tengame</h1>
         <div className='dices-container'>
           <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={3} />
-          <Die value={3} />
-          <Die value={3} />
-          <Die value={3} />
-          <Die value={3} />
-          <Die value={3} />
-          <Die value={3} />
         </div>
       </main>
     </>
