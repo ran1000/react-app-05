0b1c4e9c636c23d6d540d0a2a2834c056eff0c3d
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-09-20 23:01:00 +0100

    Updates

---
+++ precache-manifest.b3035f25912019af9025ffe9aed2ecdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9848803f128d65443e1",
+    "url": "/react-app-05/static/css/main.4a5517df.chunk.css"
+  },
+  {
+    "revision": "f9848803f128d65443e1",
+    "url": "/react-app-05/static/js/main.f9848803.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/react-app-05/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "77752ca05ed52006c843",
+    "url": "/react-app-05/static/js/2.77752ca0.chunk.js"
+  },
+  {
+    "revision": "915599d67ec0d75fb4c1",
+    "url": "/react-app-05/static/js/runtime~main.915599d6.js"
+  },
+  {
+    "revision": "1ee5385b4d053bd973106f480e9a3242",
+    "url": "/react-app-05/index.html"
+  }
+];
